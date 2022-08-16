3a67b3909e4283f2a52bfd8fb1adf54791330f37
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:16:34 +0000

    adding import argument and formatting document

--- BenchPress/Generators/Program.cs
+++ BenchPress/Generators/Program.cs
@@ -1,48 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Generators;
+﻿using Generators;
 using Generators.LanguageProviders;
 using Generators.ResourceTypes;
+using System.CommandLine;
 
-var language = new PowershellLanguageProvider();
-var generator = new TestGenerator(language);
+var fileOption = new Option<FileInfo?>(name: "--import", description: "The bicep file to import and scaffold tests for");
 
-var metadataList = AzureDeploymentImporter.Import("sample.bicep");
-var testList = new List<TestDefinition>();
+var rootCommand = new RootCommand("Test Generator for Bicep");
 
-// Resource exists tests
-foreach (var metadata in metadataList)
-{
-    testList.Add(new TestDefinition(
-        metadata,
-        TestType.ResourceExists));
+rootCommand.AddOption(fileOption);
 
-    testList.Add(new TestDefinition(
-        metadata,
-        TestType.Location));
-}
+rootCommand.SetHandler((file) =>
+  {
+    var language = new PowershellLanguageProvider();
+    var generator = new TestGenerator(language);
 
-var testGroups = new List<IEnumerable<TestDefinition>>();
+    var metadataList = AzureDeploymentImporter.Import(file);
+    var testList = new List<TestDefinition>();
 
-testGroups.Add(testList.Where(t => t.Metadata.ResourceType.GetType() == typeof(VirtualMachine)));
-testGroups.Add(testList.Where(t => t.Metadata.ResourceType.GetType() == typeof(ResourceGroup)));
-
-var templateFile = "./templates/powershell/template.ps1";
-
-foreach (var group in testGroups)
-{
-    if (!group.Any()) continue;
-
-    var testsOutput = generator.Generate(group, templateFile);
-
-    var testFileName = group.First().Metadata.ResourceType.Prefix + "Tests.ps1";
-    var testFilePath = "output";
-
-    var testFileFullName = Path.Join(testFilePath, testFileName);
-
-    if (!Directory.Exists(testFilePath))
+    foreach (var metadata in metadataList)
     {
-        Directory.CreateDirectory(testFilePath);
+      testList.Add(new TestDefinition(metadata, TestType.ResourceExists));
+      testList.Add(new TestDefinition(metadata, TestType.Location));
     }
-    
-    File.WriteAllText(testFileFullName, testsOutput);
-}
+
+    var testGroups = new List<IEnumerable<TestDefinition>>();
+
+    testGroups.Add(testList.Where(t => t.Metadata.ResourceType.GetType() == typeof(VirtualMachine)));
+    testGroups.Add(testList.Where(t => t.Metadata.ResourceType.GetType() == typeof(ResourceGroup)));
+
+    var templateFile = "./templates/powershell/template.ps1";
+
+    foreach (var group in testGroups)
+    {
+      if (!group.Any()) continue;
+
+      var testsOutput = generator.Generate(group, templateFile);
+
+      var testFileName = group.First().Metadata.ResourceType.Prefix + "Tests.ps1";
+      var testFilePath = "output";
+
+      var testFileFullName = Path.Join(testFilePath, testFileName);
+
+      if (!Directory.Exists(testFilePath))
+      {
+        Directory.CreateDirectory(testFilePath);
+      }
+
+      File.WriteAllText(testFileFullName, testsOutput);
+    }
+  },
+  fileOption
+);
+
+await rootCommand.InvokeAsync(args);
