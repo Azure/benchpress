a7b3655a535c21a3b54b9c42ef25e9518b11092e
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:18:23 +0000

    formatting document

--- BenchPress/Generators/ResourceTypes/ResourceGroup.cs
+++ BenchPress/Generators/ResourceTypes/ResourceGroup.cs
@@ -2,24 +2,22 @@ namespace Generators.ResourceTypes;
 
 public class ResourceGroup : ResourceType
 {
-    public ResourceGroup()
-    {
-    }
+  public ResourceGroup()
+  {
+  }
 
-    public const string Id = "Microsoft.Resources/resourceGroups";
+  public const string Id = "Microsoft.Resources/resourceGroups";
 
-    public override string FullName => Id;
+  public override string FullName => Id;
 
-    public override string FriendlyName => "Resource Groups";
+  public override string FriendlyName => "Resource Groups";
 
-    public override string Prefix => "rg";
+  public override string Prefix => "rg";
 
-    public override string FunctionPrefix => "ResourceGroup";
+  public override string FunctionPrefix => "ResourceGroup";
 
-    public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
-    {
-        return new [] {
-            Param("name", m.ResourceName)
-        };
-    }
+  public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+  {
+    return new[] { Param("name", m.ResourceName) };
+  }
 }
