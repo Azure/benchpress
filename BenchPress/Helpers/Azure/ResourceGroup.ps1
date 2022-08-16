c9fba2c7ef601cc0abd8ba3c52a2d997aab5bd10
Engin Polat <polatengin@hotmail.com>
2022-08-16 21:00:39 +0000

    improving azure resource group module

--- BenchPress/Helpers/Azure/ResourceGroup.ps1
+++ BenchPress/Helpers/Azure/ResourceGroup.ps1
@@ -1,9 +1,13 @@
-function Get-ResourceGroupExists([string]$resourceGroupName) {
-  $rg = Get-AzResourceGroup $resourceGroupName
-  if ($null -eq $rg) {
-    throw "Resource group $resourceGroupName was not found!"
-  }
-  else{
-    return $true
-  }
+function Get-ResourceGroup([string]$resourceGroupName) {
+  $resource = Get-AzResourceGroup $resourceGroupName
+  return $resource
 }
+
+function Get-ResourceGroupExists([string]$resourceGroupName) {
+  $resource = Get-ResourceGroup $resourceGroupName
+
+  return ($null -ne $resource)
+}
+
+Export-ModuleMember -Function `
+  Get-ResourceGroup, Get-ResourceGroupExists
