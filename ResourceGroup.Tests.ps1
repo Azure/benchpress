20c0f895f83479e13810d65171c556e798f1ad75
Engin Polat <polatengin@hotmail.com>
2022-08-16 19:36:46 +0000

    formatting document

--- ResourceGroup.Tests.ps1
+++ ResourceGroup.Tests.ps1
@@ -1,35 +1,36 @@
-BeforeAll{
-    . $PSScriptRoot/BenchPress/Helpers/Azure/ResourceGroup.ps1
-    . $PSScriptRoot/BenchPress/Helpers/Azure/Bicep.ps1
+BeforeAll {
+  . $PSScriptRoot/BenchPress/Helpers/Azure/ResourceGroup.ps1
+  . $PSScriptRoot/BenchPress/Helpers/Azure/Bicep.ps1
 }
+
 Describe 'Verify Resource Group Exists' {
-    it 'Should contain a resource group named tflintrules' {
-        #arrange
-        $rgName = "tflintrules"
+  it 'Should contain a resource group named tflintrules' {
+    #arrange
+    $rgName = "tflintrules"
 
-        #act
-        $exists = Get-ResourceGroupExists($rgName)
+    #act
+    $exists = Get-ResourceGroupExists($rgName)
 
-        #assert
-        $exists | Should -Be $true
-    } 
+    #assert
+    $exists | Should -Be $true
+  }
 }
 
 Describe 'Spin up , Tear down Resource Group' {
-    it 'Should deploy a bicep file.' {
-        #arrange
-        $bicepPath = "./examples/main.bicep"
-        $params = @{
-            name = "rgtestocw11"
-            location = "westus"
-            environment = "ocwtest"
-        }
-        #act
-        $deployment = Deploy-BicepFeature $bicepPath $params
-        #assert
-        $deployment.ProvisioningState| Should -Be "Succeeded"
+  it 'Should deploy a bicep file.' {
+    #arrange
+    $bicepPath = "./examples/main.bicep"
+    $params = @{
+      name        = "rgtestocw11"
+      location    = "westus"
+      environment = "ocwtest"
+    }
+    #act
+    $deployment = Deploy-BicepFeature $bicepPath $params
+    #assert
+    $deployment.ProvisioningState | Should -Be "Succeeded"
 
-        #clean up
-        Remove-BicepFeature $params
-    } 
+    #clean up
+    Remove-BicepFeature $params
+  }
 }
