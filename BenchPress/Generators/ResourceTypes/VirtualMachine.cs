907394901add1a85bb65a2c9a8e0ca16b866c753
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:18:43 +0000

    formatting document

--- BenchPress/Generators/ResourceTypes/VirtualMachine.cs
+++ BenchPress/Generators/ResourceTypes/VirtualMachine.cs
@@ -2,25 +2,25 @@ namespace Generators.ResourceTypes;
 
 public class VirtualMachine : ResourceType
 {
-    public VirtualMachine()
-    {
-    }
+  public VirtualMachine()
+  {
+  }
 
-    public const string Id = "Microsoft.Compute/virtualMachines";
+  public const string Id = "Microsoft.Compute/virtualMachines";
 
-    public override string FullName => Id;
+  public override string FullName => Id;
 
-    public override string FriendlyName => "Virtual Machines";
+  public override string FriendlyName => "Virtual Machines";
 
-    public override string Prefix => "vm";
+  public override string Prefix => "vm";
 
-    public override string FunctionPrefix => "VirtualMachine";
+  public override string FunctionPrefix => "VirtualMachine";
 
-    public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
-    {
-        return new [] {
-            Param("name", m.ResourceName),
-            Param("resourceGroup", m.ExtraProperties["resourceGroup"])
-        };
-    }
+  public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+  {
+    return new[] {
+      Param("name", m.ResourceName),
+      Param("resourceGroup", m.ExtraProperties["resourceGroup"])
+    };
+  }
 }
