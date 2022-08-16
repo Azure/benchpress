b534ba45221cb2158aefce50624f112c1b683a80
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:18:32 +0000

    formatting document

--- BenchPress/Generators/ResourceTypes/ResourceType.cs
+++ BenchPress/Generators/ResourceTypes/ResourceType.cs
@@ -2,37 +2,36 @@ namespace Generators.ResourceTypes;
 
 public abstract class ResourceType
 {
-    internal const string ResourceGroupTypeId = "Microsoft.Resources/resourceGroups";
+  internal const string ResourceGroupTypeId = "Microsoft.Resources/resourceGroups";
 
-    public static ResourceType Create(string resourceType)
+  public static ResourceType Create(string resourceType)
+  {
+    switch (resourceType)
     {
-        switch (resourceType)
-        {
-            case ResourceGroup.Id:
-                return new ResourceGroup();
-            case VirtualMachine.Id:
-                return new VirtualMachine();
+      case ResourceGroup.Id:
+        return new ResourceGroup();
+      case VirtualMachine.Id:
+        return new VirtualMachine();
 
-            default:
-                throw new UnknownResourceTypeException(resourceType);
-        }
+      default:
+        throw new UnknownResourceTypeException(resourceType);
     }
-    public abstract string FullName { get; }
-    public abstract string FriendlyName { get; }
-    public abstract string Prefix { get; }
-    public abstract string FunctionPrefix { get; }
-    public abstract IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m);
+  }
+  public abstract string FullName { get; }
+  public abstract string FriendlyName { get; }
+  public abstract string Prefix { get; }
+  public abstract string FunctionPrefix { get; }
+  public abstract IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m);
 
-    protected static KeyValuePair<string, object> Param(string name, object value)
+  protected static KeyValuePair<string, object> Param(string name, object value)
+  {
+    return new KeyValuePair<string, object>(name, value);
+  }
+
+  public class UnknownResourceTypeException : Exception
+  {
+    public UnknownResourceTypeException(string resourceType) : base($"Unknown resource type: {resourceType}")
     {
-        return new KeyValuePair<string, object>(name, value);
     }
-
-    public class UnknownResourceTypeException : Exception
-    {
-        public UnknownResourceTypeException(string resourceType) : base($"Unknown resource type: {resourceType}")
-        {
-        }
-    }
+  }
 }
-
