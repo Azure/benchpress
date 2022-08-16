d9bc6ba5e21638a6a615662a7c38bb34c87a76ca
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:15:41 +0000

    formatting document

--- BenchPress/Generators/TestMetadata.cs
+++ BenchPress/Generators/TestMetadata.cs
@@ -4,13 +4,13 @@ namespace Generators;
 
 public class TestMetadata
 {
-    public TestMetadata(string resourceType, string resourceName, IDictionary<string, object> extraProperties)
-    {
-        ResourceType = ResourceType.Create(resourceType);
-        ResourceName = resourceName;
-        ExtraProperties = extraProperties;
-    }
-    public ResourceType ResourceType { get; set; }
-    public string ResourceName { get; set; }
-    public IDictionary<string, object> ExtraProperties { get; set; }
+  public TestMetadata(string resourceType, string resourceName, IDictionary<string, object> extraProperties)
+  {
+    ResourceType = ResourceType.Create(resourceType);
+    ResourceName = resourceName;
+    ExtraProperties = extraProperties;
+  }
+  public ResourceType ResourceType { get; set; }
+  public string ResourceName { get; set; }
+  public IDictionary<string, object> ExtraProperties { get; set; }
 }
