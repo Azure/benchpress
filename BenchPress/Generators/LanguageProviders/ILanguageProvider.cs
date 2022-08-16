4e8f99ed5193a38bc8aaead68e023c454e604ff0
Engin Polat <polatengin@hotmail.com>
2022-08-16 22:19:29 +0000

    formatting document

--- BenchPress/Generators/LanguageProviders/ILanguageProvider.cs
+++ BenchPress/Generators/LanguageProviders/ILanguageProvider.cs
@@ -4,11 +4,11 @@ namespace Generators.LanguageProviders;
 
 public interface ILanguageProvider
 {
-    public string Function(string name);
-    public string Escape(string value);
-    public string Variable(string name);
-    public string Value(object value);
-    public string Library(ResourceType resourceType);
-    public string SDK(SDKFunction sdkFunction);
-    public string ParameterList(params string[] parameters);
+  public string Function(string name);
+  public string Escape(string value);
+  public string Variable(string name);
+  public string Value(object value);
+  public string Library(ResourceType resourceType);
+  public string SDK(SDKFunction sdkFunction);
+  public string ParameterList(params string[] parameters);
 }
