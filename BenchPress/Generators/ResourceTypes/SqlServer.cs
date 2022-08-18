6237912d1b1949476e230cf29fa65812fbae5830
Engin Polat <polatengin@hotmail.com>
2022-08-18 00:24:03 +0000

    adding sqlserver resource type

---
+++ BenchPress/Generators/ResourceTypes/SqlServer.cs
@@ -0,0 +1,26 @@
+namespace Generators.ResourceTypes;
+
+public class SqlServer : ResourceType
+{
+  public SqlServer()
+  {
+  }
+
+  public const string Id = "Microsoft.Sql/servers";
+
+  public override string FullName => Id;
+
+  public override string FriendlyName => "SQL Server";
+
+  public override string Prefix => "sqlserver";
+
+  public override string FunctionPrefix => "SqlServer";
+
+  public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+  {
+    return new[] {
+      Param("name", m.ResourceName),
+      Param("resourceGroup", m.ExtraProperties["resourceGroup"])
+    };
+  }
+}
