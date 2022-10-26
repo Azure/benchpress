5572ced9ede7770b1c95b95a49f19233f2677d1e
Uffaz <uffaz@Uffazs-MacBook-Pro.local>
2022-10-26 16:55:25 -0700

    Create an installable benchpress python module. Configure dev container. Update documentation on how to use Python module

---
+++ framework/python/src/benchpress/calculator.py
@@ -0,0 +1,13 @@
+
+
+def add(num1: int, num2: int) -> int:
+    """Adds two numbers together and returns their results
+
+    Args:
+        num1 (int): The first number to add
+        num2 (int): The second number to add
+
+    Returns:
+        int: The computed result of `num1 + num2`
+    """
+    return num1 + num2
---
+++ framework/python/tests/test_calculator.py
@@ -0,0 +1,8 @@
+from benchpress import add
+
+
+def test_add():
+    """
+    Test benchpress's add method
+    """
+    assert add(1, 2) == 3
