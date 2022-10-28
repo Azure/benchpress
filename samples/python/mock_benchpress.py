eeee250e81da22008554524aac7af6583e5738fb
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2022-10-28 12:32:54 -0500

    Feature/python sample tests (#27)
    * first draft of python sample code for running basic benchpress test with most things mocked out
    
    * add basic sample python test with many things still mocked
    
    * update python sample code to be more conformant with issue description
    
    * minor improvements and bug fixes
    
    * configure python linters to ignore try,except,pass or no positive except assertion because we are at the mocking stage
    
    * remove unused lint config files (#26)
    
    Co-authored-by: Robert David Hernandez <robhernandez@microsoft.com>
    
    * Create an installable benchpress python module. Configure dev container. Update documentation on how to use Python module
    
    * Revert "Create an installable benchpress python module. Configure dev container. Update documentation on how to use Python module"
    
    This reverts commit 5572ced9ede7770b1c95b95a49f19233f2677d1e.
    
    * update config directory vars in .mega-linter.yml from pointers to indv files to single pointer to parent dir (#32)
    
    * remove unused lint config files
    
    * update config directory vars from pointers to indv files to single pointer to parent dir
    
    Co-authored-by: Robert David Hernandez <robhernandez@microsoft.com>
    
    * Feature/pws sample (#28)
    
    * working on workflow
    
    * workig document workfow
    
    * dotnet lint rules
    
    * test python workflow
    
    * test python workflow
    
    * test python workflow
    
    * pr_python
    
    * pr_worflow check pyhton and dotnet
    
    * test workflow
    
    * test workflow
    
    * test workflow
    
    * test workflow
    
    * test pyhton
    
    * test pyhton
    
    * test python
    
    * pr_doc workflow
    
    * docs check
    
    * docs check
    
    * docs check
    
    * pr_workflow
    
    * pr_workflow
    
    * pr_workflow
    
    * pr_workflow
    
    * link check edit
    
    * link check edit
    
    * link check edit
    
    * link check edit
    
    * link check edit
    
    * link check edit
    
    * corrections to pr md files
    
    * cspell configuration
    
    * cspell configuration
    
    * cspell configuration
    
    * cspell configuration
    
    * doc file update and add config for markdown-link-check
    
    * doc file update and add config for markdown-link-check
    
    * change folder structure for linter config files
    
    * fix documentation
    
    * fix documentation
    
    * resolve document misspell
    
    * resolve editorconfig for markdwon
    
    * resolve editorconfig for markdwon
    
    * resolve editorconfig for markdwon
    
    * powershell test sample
    
    * powershell test sample
    
    * fix dotnet issue
    
    * powershell changes
    
    * storage account and bicep helpers
    
    * storage account and bicep helpers
    
    * PowerShell Storage Account test sample
    
    * PowerShell Storage Account test sample
    
    * PowerShell Storage Account test sample
    
    * PowerShell Storage Account test sample
    
    * PowerShell Storage Account test sample
    
    * code cleanup
    
    * updated documentation
    
    * updated documentation
    
    * updated test file
    
    * remove .pylintrc
    
    * configure python linters to ignore try,except,pass or no positive except assertion because we are at the mocking stage
    
    * update bandit and pylintrc linting rules to ignore try,except,pass
    
    * attempt to bypass try-except-pass lint fail with a no-op
    
    Co-authored-by: Uffaz <uffaz@Uffazs-MacBook-Pro.local>
    Co-authored-by: Dilmurod Makhamadaliev <104784252+DilmurodMak@users.noreply.github.com>

---
+++ samples/python/mock_benchpress.py
@@ -0,0 +1,11 @@
+def resource_group_exists(resource_group: str) -> bool:
+    return True
+
+def create_resource_group(azure_subscription: str) -> str:
+    return "dummy_resource_group_id"
+
+def destroy(resource_group: str):
+    pass
+
+if __name__ == "__main__":
+    pass
