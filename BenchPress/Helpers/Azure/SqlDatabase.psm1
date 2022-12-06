af1835a577fb1d5c35685f5dd66eaaf41d2b7efe
Engin Polat <polatengin@hotmail.com>
2022-12-06 21:21:54 +0000

    updating sql database module to enable helper functions callable from powershell directly

--- BenchPress/Helpers/Azure/SqlDatabase.psm1
+++ BenchPress/Helpers/Azure/SqlDatabase.psm1
@@ -1,9 +1,32 @@
-function Get-SqlDatabase([string]$databaseName, [string]$serverName, [string]$resourceGroupName) {
+function Get-SqlDatabase {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$databaseName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$serverName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName
+  )
+
   $resource = Get-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName
   return $resource
 }
 
-function Get-SqlDatabaseExists([string]$databaseName, [string]$serverName, [string]$resourceGroupName) {
+function Get-SqlDatabaseExists {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$databaseName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$serverName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName
+  )
   $resource = Get-SqlDatabase $databaseName $serverName $resourceGroupName
   return ($null -ne $resource)
 }
