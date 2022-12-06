ec330d3c8a437b266a33c976cfea51523d8a61eb
Engin Polat <polatengin@hotmail.com>
2022-12-06 21:29:24 +0000

    updating sql server module to enable helper functions callable from powershell directly

--- BenchPress/Helpers/Azure/SqlServer.psm1
+++ BenchPress/Helpers/Azure/SqlServer.psm1
@@ -1,9 +1,27 @@
-function Get-SqlServer([string]$serverName, [string]$resourceGroupName) {
+function Get-SqlServer {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$serverName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName
+  )
+
   $resource = Get-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName
   return $resource
 }
 
-function Get-SqlServerExists([string]$serverName, [string]$resourceGroupName) {
+function Get-SqlServerExists {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$serverName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName
+  )
+
   $resource = Get-SqlServer $serverName $resourceGroupName
   return ($null -ne $resource)
 }
