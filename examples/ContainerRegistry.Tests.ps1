8960ca2afbf7d92c3776104e089028ce31b213d9
Engin Polat <polatengin@hotmail.com>
2022-12-08 01:10:37 +0000

    adding deploy-destroy container registry test

--- examples/ContainerRegistry.Tests.ps1
+++ examples/ContainerRegistry.Tests.ps1
@@ -30,3 +30,21 @@ Describe 'Verify Container Registry Exists' {
     $exists | Should -Be $true
   }
 }
+
+Describe 'Spin up , Tear down Container Registry' {
+  it 'Should deploy a bicep file.' {
+    #arrange
+    $bicepPath = "./containerRegistry.bicep"
+    $params = @{
+      name           = "acrbenchpresstest1"
+      location       = "westus3"
+    }
+    #act
+    $deployment = Deploy-BicepFeature $bicepPath $params "rgtest"
+    #assert
+    $deployment.ProvisioningState | Should -Be "Succeeded"
+
+    #clean up
+    Remove-BicepFeature "rgtest"
+  }
+}
