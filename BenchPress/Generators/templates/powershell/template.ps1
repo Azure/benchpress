9568a5798e09d4656152dc3339f74ca59b57be31
Mary Sha (SHE/HER) <meisha@microsoft.com>
2023-01-26 09:27:43 -0800

    Resolve some powershell linting issues

--- BenchPress/Generators/templates/powershell/template.ps1
+++ BenchPress/Generators/templates/powershell/template.ps1
@@ -1,4 +1,10 @@
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments',
+                                                          'This is an auto generated file by the Generator and should
+                                                           be ignored for now as the Generator is still under
+                                                           development.')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UnexpectedToken', '')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('MissingEndCurlyBrace', '')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('InvalidLeftHandSide', '')]
 param()
 
 BeforeAll {
