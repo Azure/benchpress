27a84c7e4e0bc3d3bdff596186980b6d6ca06d99
Mary Sha (SHE/HER) <meisha@microsoft.com>
2023-01-26 17:03:49 -0800

    Resolve csharpier issues

--- BenchPress/Generators/ResourceTypes/AppService.cs
+++ BenchPress/Generators/ResourceTypes/AppService.cs
@@ -2,25 +2,24 @@ namespace Generators.ResourceTypes;
 
 public class AppService : ResourceType
 {
-  public AppService()
-  {
-  }
+    public AppService() { }
 
-  public override string Id => "Microsoft.Web/sites";
+    public override string Id => "Microsoft.Web/sites";
 
-  public override string FullName => Id;
+    public override string FullName => Id;
 
-  public override string FriendlyName => "App Service";
+    public override string FriendlyName => "App Service";
 
-  public override string Prefix => "app";
+    public override string Prefix => "app";
 
-  public override string FunctionPrefix => "AppService";
+    public override string FunctionPrefix => "AppService";
 
-  public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
-  {
-    return new[] {
-      Param("name", m.ResourceName),
-      Param("resourceGroup", m.ExtraProperties["resourceGroup"])
-    };
-  }
+    public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+    {
+        return new[]
+        {
+            Param("name", m.ResourceName),
+            Param("resourceGroup", m.ExtraProperties["resourceGroup"])
+        };
+    }
 }
--- BenchPress/Generators/ResourceTypes/ResourceGroup.cs
+++ BenchPress/Generators/ResourceTypes/ResourceGroup.cs
@@ -2,22 +2,20 @@ namespace Generators.ResourceTypes;
 
 public class ResourceGroup : ResourceType
 {
-  public ResourceGroup()
-  {
-  }
+    public ResourceGroup() { }
 
-  public override string Id => "Microsoft.Resources/resourceGroups";
+    public override string Id => "Microsoft.Resources/resourceGroups";
 
-  public override string FullName => Id;
+    public override string FullName => Id;
 
-  public override string FriendlyName => "Resource Groups";
+    public override string FriendlyName => "Resource Groups";
 
-  public override string Prefix => "rg";
+    public override string Prefix => "rg";
 
-  public override string FunctionPrefix => "ResourceGroup";
+    public override string FunctionPrefix => "ResourceGroup";
 
-  public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
-  {
-    return new[] { Param("name", m.ResourceName) };
-  }
+    public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+    {
+        return new[] { Param("name", m.ResourceName) };
+    }
 }
