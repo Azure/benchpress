d1931b4acf2dffe0857875fade4437eb62da3114
Engin Polat <enpolat@microsoft.com>
2023-02-09 17:02:19 +0000

    if required environment variables are not set, use default values

--- assets/demos/deploy-demoapp.ps1
+++ assets/demos/deploy-demoapp.ps1
@@ -1,11 +1,14 @@
-az group create --name "benchpress-rg-${env:ENVIRONMENT_SUFFIX}" --location "${env:LOCATION}"
+$ENVIRONMENT_SUFFIX = $env:ENVIRONMENT_SUFFIX ?? "benchpress-rg-$((Get-Random).ToString("x8"))";
+$LOCATION = $env:LOCATION ?? "westus2";
 
-az deployment group create --resource-group "benchpress-rg-${env:ENVIRONMENT_SUFFIX}" --template-file "main.bicep" --parameters suffix="${env:ENVIRONMENT_SUFFIX}"
+az group create --name "benchpress-rg-${ENVIRONMENT_SUFFIX}" --location "${LOCATION}"
+
+az deployment group create --resource-group "benchpress-rg-${ENVIRONMENT_SUFFIX}" --template-file "main.bicep" --parameters suffix="${ENVIRONMENT_SUFFIX}"
 
 git clone https://github.com/dotnet/AspNetCore.Docs.git
 
 Push-Location -Path .\AspNetCore.Docs\aspnetcore\mvc\controllers\filters\samples\6.x\FiltersSample
 
-az webapp up --name "benchpress-web-${env:ENVIRONMENT_SUFFIX}" --resource-group "benchpress-rg-${env:ENVIRONMENT_SUFFIX}" --location "${env:LOCATION}" --sku "F1"
+az webapp up --name "benchpress-web-${ENVIRONMENT_SUFFIX}" --resource-group "benchpress-rg-${ENVIRONMENT_SUFFIX}" --location "${LOCATION}" --sku "F1"
 
 Pop-Location
