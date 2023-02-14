66f537e5b6e753b30c4c761e53f26abbe9d574ac
Engin Polat <enpolat@microsoft.com>
2023-02-14 01:27:19 +0000

    splitting code into functions to maintain easier

--- assets/demos/deploy-demoapp.ps1
+++ assets/demos/deploy-demoapp.ps1
@@ -1,14 +1,81 @@
-$ENVIRONMENT_SUFFIX = $env:ENVIRONMENT_SUFFIX ?? "benchpress-rg-$((Get-Random).ToString("x8"))";
+<#
+.DESCRIPTION
+  Creates a resource group with given name, location and tags.
+#>
+function New-ResourceGroup {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+    [Parameter(Mandatory=$true)]
+    [string]$Location,
+    [Parameter(Mandatory=$true)]
+    [string]$Tags
+  )
+
+  az group create --name $Name --location $Location --tags $Tags
+}
+
+<#
+.DESCRIPTION
+  Creates a deployment.
+#>
+function New-Deployment {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [string]$TemplateFile,
+    [Parameter(Mandatory=$true)]
+    [string]$Parameters
+  )
+
+  az deployment group create --resource-group $ResourceGroup --template-file $TemplateFile --parameters $Parameters
+}
+
+<#
+.DESCRIPTION
+  Deploys the demo app.
+#>
+
+function Deploy-DemoApp {
+  [CmdletBinding()]
+  param(
+    [Switch]$Force,
+    [Parameter(Mandatory=$true)]
+    [string]$EnvironmentSuffix,
+    [Parameter(Mandatory=$true)]
+    [string]$Location
+  )
+
+  if ($Force -eq $false) {
+    Write-Information "This script will create a resource group, a deployment and a web app. If you want to force the execution, use the -Force switch."
+    Write-Information ""
+    Write-Information "Resources that will be created:"
+    Write-Information "  - Resource group: benchpress-rg-${EnvironmentSuffix}"
+    Write-Information "  - App Service Plan: benchpress-hosting-plan-${EnvironmentSuffix}"
+    Write-Information "  - Web app: benchpress-web-${EnvironmentSuffix}"
+    Write-Information "  - Application Insights: benchpress-application-insights-${EnvironmentSuffix}"
+    Write-Information "  - Email Action Group: benchpress-email-action-group-${EnvironmentSuffix}"
+    Write-Information ""
+    return
+  }
+
+  New-ResourceGroup -Name "benchpress-rg-${EnvironmentSuffix}" -Location $Location -Tags "application=benchpress-demo suffix=${EnvironmentSuffix}"
+
+  New-Deployment -ResourceGroup "benchpress-rg-${EnvironmentSuffix}" -TemplateFile "main.bicep" -Parameters "suffix=${EnvironmentSuffix}"
+
+  git clone https://github.com/dotnet/AspNetCore.Docs.git
+
+  Push-Location -Path .\AspNetCore.Docs\aspnetcore\mvc\controllers\filters\samples\6.x\FiltersSample
+
+  az webapp up --name "benchpress-web-${EnvironmentSuffix}" --resource-group "benchpress-rg-${EnvironmentSuffix}" --location "${Location}" --sku "F1"
+
+  Pop-Location
+}
+
+$ENVIRONMENT_SUFFIX = $env:ENVIRONMENT_SUFFIX ?? (Get-Random).ToString("x8")
 $LOCATION = $env:LOCATION ?? "westus2";
 
-az group create --name "benchpress-rg-${ENVIRONMENT_SUFFIX}" --location "${LOCATION}" --tags "application=benchpress-demo suffix=${ENVIRONMENT_SUFFIX}"
-
-az deployment group create --resource-group "benchpress-rg-${ENVIRONMENT_SUFFIX}" --template-file "main.bicep" --parameters suffix="${ENVIRONMENT_SUFFIX}"
-
-git clone https://github.com/dotnet/AspNetCore.Docs.git
-
-Push-Location -Path .\AspNetCore.Docs\aspnetcore\mvc\controllers\filters\samples\6.x\FiltersSample
-
-az webapp up --name "benchpress-web-${ENVIRONMENT_SUFFIX}" --resource-group "benchpress-rg-${ENVIRONMENT_SUFFIX}" --location "${LOCATION}" --sku "F1"
-
-Pop-Location
+Deploy-DemoApp -EnvironmentSuffix $ENVIRONMENT_SUFFIX -Location $LOCATION
