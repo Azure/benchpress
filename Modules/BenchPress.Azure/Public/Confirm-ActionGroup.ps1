6b0c4b56244ac764a4d4988bac86253018cacdfa
Mary Sha (SHE/HER) <meisha@microsoft.com>
2023-03-10 10:14:24 -0800

    Force a change

--- Modules/BenchPress.Azure/Public/Confirm-ActionGroup.ps1
+++ Modules/BenchPress.Azure/Public/Confirm-ActionGroup.ps1
@@ -7,7 +7,7 @@ using module ./../Classes/ConfirmResult.psm1
 function Confirm-ActionGroup {
   <#
     .SYNOPSIS
-      Confirms that an Action Group exists. Hello World! I'm changing!
+      Confirms that an Action Group exists. Hello World! I'm changing! Nevermind, this is a better change!
 
     .DESCRIPTION
       The Confirm-AzBPActionGroup cmdlet gets an action group using the specified Action Group and Resource Group name.
