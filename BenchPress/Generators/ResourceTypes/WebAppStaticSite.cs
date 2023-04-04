4241eba6e3c2e04ab3d619ada954619913d7409a
Caroline <caquigg@microsoft.com>
2023-04-04 13:21:34 -0700

    add generator class

---
+++ BenchPress/Generators/ResourceTypes/WebAppStaticSite.cs
@@ -0,0 +1,22 @@
+namespace Generators.ResourceTypes;
+
+public class WebAppStaticSite : ResourceType
+{
+    public WebAppStaticSite() { }
+
+    public override string Id => "Microsoft.Web/staticSites";
+    public override string FullName => Id;
+    public override string FriendlyName => "Web Application Static Site";
+    public override string Prefix => "wass";
+    public override string FunctionPrefix => "WebAppStaticSite";
+
+    public override IEnumerable<KeyValuePair<string, object>> GetResourceParameters(TestMetadata m)
+    {
+        return new[]
+        {
+            Param("ResourceType", "WebAppStaticSite"),
+            Param("ResourceName", m.ResourceName),
+            Param("ResourceGroupName", m.ExtraProperties["resourceGroup"])
+        };
+    }
+}
