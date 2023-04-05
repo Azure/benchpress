3515690c95a3216f68f3cb225782017012eed866
Mary Sha (SHE/HER) <meisha@microsoft.com>
2023-04-04 17:40:50 -0700

    Initial scaffolding for dashboard support

---
+++ Modules/BenchPress.Azure/Public/Confirm-PortalDashboard.ps1
@@ -0,0 +1,48 @@
+﻿# INLINE_SKIP
+using module ./../Classes/ConfirmResult.psm1
+
+. $PSScriptRoot/../Private/Connect-Account.ps1
+# end INLINE_SKIP
+
+function Confirm-PortalDashboard {
+  <#
+    .SYNOPSIS
+      Confirms that an Azure Dashboard exists.
+
+    .DESCRIPTION
+      The Confirm-AzBPPortalDashboard cmdlet gets an Azure Dashboard using the specified Dashboard and Resource Group names.
+
+    .PARAMETER Name
+      The name of the Azure Dashboard.
+
+    .PARAMETER ResourceGroupName
+      The name of the Resource Group. The name is case insensitive.
+
+    .EXAMPLE
+      Confirm-AzBPPortalDashboard -Name "benchpresstest" -ResourceGroupName "rgbenchpresstest"
+
+    .INPUTS
+      System.String
+
+    .OUTPUTS
+      ConfirmResult
+  #>
+  [CmdletBinding()]
+  [OutputType([ConfirmResult])]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroupName
+  )
+  Begin {
+    $connectResults = Connect-Account
+  }
+  Process {
+    $resource = Get-AzPortalDashboard -ResourceGroupName $ResourceGroupName -Name $Name
+
+    [ConfirmResult]::new($resource, $connectResults.AuthenticationData)
+  }
+  End { }
+}
---
+++ examples/Dashboard/Dashboard.Tests.ps1
@@ -0,0 +1,66 @@
+﻿BeforeAll {
+  Import-Module Az.InfrastructureTesting
+
+  $Script:rgName   = 'rg-test'
+  $Script:location = 'westus3'
+  $Script:dashboardName = 'sampleDashboard'
+}
+
+Describe 'Verify Dashboard' {
+  BeforeAll {
+    $Script:noDashboardName = 'noDashboard'
+  }
+
+  It "Should contain a Dashboard named $dashboardName - Confirm-AzBPResource" {
+    # arrange
+    $params = @{
+      ResourceType      = "PortalDashboard"
+      ResourceName      = $dashboardName
+      ResourceGroupName = $rgName
+    }
+
+    # act and assert
+    Confirm-AzBPResource @params | Should -BeSuccessful
+  }
+
+  It "Should contain a Dashboard named $dashboardName - Confirm-AzBPResource" {
+    # arrange
+    $params = @{
+      ResourceType      = "PortalDashboard"
+      ResourceName      = $dashboardName
+      ResourceGroupName = $rgName
+      PropertyKey       = "Name"
+      PropertyValue     = $dashboardName
+    }
+
+    # act and assert
+    Confirm-AzBPResource @params | Should -BeSuccessful
+  }
+
+  It "Should contain a Dashboard named $dashboardName" {
+    Confirm-AzBPPortalDashboard -ResourceGroupName $rgName -Name $dashboardName | Should -BeSuccessful
+  }
+
+  It "Should not contain a Dashboard named $noDashboardName" {
+    # arrange
+    # The '-ErrorAction SilentlyContinue' command suppresses all errors.
+    # In this test, it will suppress the error message when a resource cannot be found.
+    # Remove this field to see all errors.
+    $params = @{
+      ResourceGroupName = $rgName
+      Name              = $noDashboardName
+      ErrorAction       = 'SilentlyContinue'
+    }
+
+    # act and assert
+    Confirm-AzBPPortalDashboard @params | Should -Not -BeSuccessful
+  }
+
+  It "Should contain a Dashboard named $dashboardName in $location" {
+    Confirm-AzBPPortalDashboard -ResourceGroupName $rgName -Name $dashboardName | Should -BeInLocation $location
+  }
+
+  It "Should contain a Dashboard named $dashboardName in $rgName" {
+    Confirm-AzBPPortalDashboard -ResourceGroupName $rgName -Name $dashboardName | Should -BeInResourceGroup $rgName
+  }
+}
