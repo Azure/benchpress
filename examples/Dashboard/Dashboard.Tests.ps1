243dc68f45fa845c1f98b3748c4648074d68d200
Mary Sha (SHE/HER) <meisha@microsoft.com>
2023-04-05 09:41:40 -0700

    Remove negative test

--- examples/Dashboard/Dashboard.Tests.ps1
+++ examples/Dashboard/Dashboard.Tests.ps1
@@ -7,10 +7,6 @@
 }
 
 Describe 'Verify Dashboard' {
-  BeforeAll {
-    $Script:noDashboardName = 'noDashboard'
-  }
-
   It "Should contain a Dashboard named $dashboardName - Confirm-AzBPResource" {
     # arrange
     $params = @{
@@ -39,21 +35,6 @@ Describe 'Verify Dashboard' {
 
   It "Should contain a Dashboard named $dashboardName" {
     Confirm-AzBPPortalDashboard -ResourceGroupName $rgName -Name $dashboardName | Should -BeSuccessful
-  }
-
-  It "Should not contain a Dashboard named $noDashboardName" {
-    # arrange
-    # The '-ErrorAction SilentlyContinue' command suppresses all errors.
-    # In this test, it will suppress the error message when a resource cannot be found.
-    # Remove this field to see all errors.
-    $params = @{
-      ResourceGroupName = $rgName
-      Name              = $noDashboardName
-      ErrorAction       = 'SilentlyContinue'
-    }
-
-    # act and assert
-    Confirm-AzBPPortalDashboard @params | Should -Not -BeSuccessful
   }
 
   It "Should contain a Dashboard named $dashboardName in $location" {
