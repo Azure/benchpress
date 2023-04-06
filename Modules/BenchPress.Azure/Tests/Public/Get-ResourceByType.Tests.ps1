8892360ff01a7eb9b47dcbd82a44a997d080647a
Caroline <caquigg@microsoft.com>
2023-04-06 15:23:06 -0700

    test get resource by type with every resource

--- Modules/BenchPress.Azure/Tests/Public/Get-ResourceByType.Tests.ps1
+++ Modules/BenchPress.Azure/Tests/Public/Get-ResourceByType.Tests.ps1
@@ -1,34 +1,56 @@
-﻿BeforeAll {
-  . $PSScriptRoot/../../Public/Confirm-AppServicePlan.ps1
-  . $PSScriptRoot/../../Public/Confirm-ResourceGroup.ps1
-  . $PSScriptRoot/../../Public/Confirm-SqlDatabase.ps1
-  . $PSScriptRoot/../../Public/Confirm-SqlServer.ps1
-  . $PSScriptRoot/../../Public/Confirm-VirtualMachine.ps1
-  . $PSScriptRoot/../../Public/Confirm-WebApp.ps1
+﻿using module ./../../Classes/ResourceType.psm1
+
+BeforeAll {
   . $PSScriptRoot/../../Public/Get-ResourceByType.ps1
+  $testCases = @()
+
+  foreach ($i in [ResourceType].GetEnumNames())
+  {
+    $functionName = "Confirm-$i"
+    $fileName = "$functionName.ps1"
+
+    # dot-source the powershell function
+    . $PSScriptRoot/../../Public/$fileName
+
+    # build test case array
+    $testObject = @{
+      ResourceType = $resource
+      Expected = $functionName
+    }
+
+    $testCases+=$testObject
+  }
 }
 
 Describe "Get-ResourceByType" {
   Context "unit tests" -Tag "Unit" {
     BeforeEach {
-      Mock Confirm-ResourceGroup{}
-      Mock Confirm-AppServicePlan{}
-      Mock Confirm-SqlDatabase{}
-      Mock Confirm-SqlServer{}
-      Mock Confirm-VirtualMachine{}
-      Mock Confirm-WebApp{}
+      foreach ($i in [ResourceType].GetEnumNames())
+      {
+        $functionName = "Confirm-$i"
+        Mock $functionName {}
+      }
     }
 
-    It "Calls <expected> when <resourceType> is used" -TestCases @(
-      @{ ResourceType = "ResourceGroup"; Expected = "Confirm-ResourceGroup"}
-      @{ ResourceType = "AppServicePlan"; Expected = "Confirm-AppServicePlan"}
-      @{ ResourceType = "SqlDatabase"; Expected = "Confirm-SqlDatabase"}
-      @{ ResourceType = "SqlServer"; Expected = "Confirm-SqlServer"}
-      @{ ResourceType = "VirtualMachine"; Expected = "Confirm-VirtualMachine"}
-      @{ ResourceType = "WebApp"; Expected = "Confirm-WebApp"}
-    ) {
-      Get-ResourceByType -ResourceName resource -ResourceGroupName group -ResourceType $ResourceType -ServerName server
-      Should -Invoke -CommandName $Expected -Times 1
+    It "Calls <expected> when <resourceType> is used" -TestCases $testCases {
+      $params = @{
+        ResourceName = 'resourcename'
+        ResourceGroupName = 'resourcegroupname'
+        ResourceType = $ResourceType
+        ServerName = 'servername'
+        DataFactoryName = 'datafactoryname'
+        NamespaceName = 'namespacename'
+        EventHubName = 'eventhubname'
+        WorkspaceName = 'workspacename'
+        AccountName = 'accountname'
+        ServicePrincipalId = 'serviceprincipalid'
+        Scope = '/subscriptions/'
+        RoleDefinitionName = 'roledefinitionname'
+        ServiceName = 'servicename'
+        ClusterName = 'clustername'
+        JobName = 'jobname'
+      }
+      Get-ResourceByType @params | Should -Invoke -CommandName $Expected -Times 1
     }
   }
 }
