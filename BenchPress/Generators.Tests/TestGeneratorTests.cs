0839b274a8a3d8d53c9ccd884a17594f97605d7c
Caroline <caquigg@microsoft.com>
2023-04-12 15:02:24 -0700

    update indentation for test generator tests

--- BenchPress/Generators.Tests/TestGeneratorTests.cs
+++ BenchPress/Generators.Tests/TestGeneratorTests.cs
@@ -2,26 +2,26 @@
 
 namespace Generators.Tests
 {
-  [TestClass]
-  public class TestGeneratorTests
-  {
-    [TestMethod]
-    public void Should_Throw_If_Unknown_TestType()
+    [TestClass]
+    public class TestGeneratorTests
     {
-      // arrange
-      var resourceType = "Microsoft.Insights/actionGroups";
-      var resourceName = "Name";
-      var extraProperties = new Dictionary<string, string>();
+        [TestMethod]
+        public void Should_Throw_If_Unknown_TestType()
+        {
+            // arrange
+            var resourceType = "Microsoft.Insights/actionGroups";
+            var resourceName = "Name";
+            var extraProperties = new Dictionary<string, string>();
 
-      var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
-      var definition = new TestDefinition(metadata, (TestType)1);
+            var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
+            var definition = new TestDefinition(metadata, (TestType)1);
 
-      var generator = new TestGenerator(new PowershellLanguageProvider());
-      var templateFile = "./templates/powershell/template.ps1";
+            var generator = new TestGenerator(new PowershellLanguageProvider());
+            var templateFile = "./templates/powershell/template.ps1";
 
-      // act and assert
-      Action action = () => { generator.Generate(new List<TestDefinition> { definition }, templateFile); };
-      Assert.ThrowsException<Exception>(action);
+            // act and assert
+            Action action = () => { generator.Generate(new List<TestDefinition> { definition }, templateFile); };
+            Assert.ThrowsException<Exception>(action);
+        }
     }
-  }
 }
