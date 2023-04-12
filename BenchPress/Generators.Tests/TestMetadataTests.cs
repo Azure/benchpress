2b2d50901e38e2e364521b63955b9086d9120728
Caroline <caquigg@microsoft.com>
2023-04-12 15:16:20 -0700

    update indentation for test classes

--- BenchPress/Generators.Tests/TestMetadataTests.cs
+++ BenchPress/Generators.Tests/TestMetadataTests.cs
@@ -1,34 +1,34 @@
 ﻿namespace Generators.Tests
 {
-  [TestClass]
-  public class TestMetadataTests
+    [TestClass]
+    public class TestMetadataTests
   {
-    [TestMethod]
-    public void Should_Throw_If_Unknown_ResourceType()
-    {
-      // arrange
-      var resourceType = "Unknown";
-      var resourceName = "Name";
-      var extraProperties = new Dictionary<string, string>();
+      [TestMethod]
+      public void Should_Throw_If_Unknown_ResourceType()
+      {
+          // arrange
+          var resourceType = "Unknown";
+          var resourceName = "Name";
+          var extraProperties = new Dictionary<string, string>();
 
-      // act and assert
-      Action action = () => { new TestMetadata(resourceType, resourceName, extraProperties); };
-      Assert.ThrowsException<UnknownResourceTypeException>(action);
-    }
+          // act and assert
+          Action action = () => { new TestMetadata(resourceType, resourceName, extraProperties); };
+          Assert.ThrowsException<UnknownResourceTypeException>(action);
+      }
 
-    [TestMethod]
-    public void Should_Succeed_If_ResourceType_Exists()
-    {
-      // arrange
-      var resourceType = "Microsoft.Insights/actionGroups";
-      var resourceName = "Name";
-      var extraProperties = new Dictionary<string, string>();
+      [TestMethod]
+      public void Should_Succeed_If_ResourceType_Exists()
+      {
+          // arrange
+          var resourceType = "Microsoft.Insights/actionGroups";
+          var resourceName = "Name";
+          var extraProperties = new Dictionary<string, string>();
 
-      // act
-      var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
+          // act
+          var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
 
-      // assert
-      Assert.IsNotNull(metadata);
-    }
+          // assert
+          Assert.IsNotNull(metadata);
+      }
   }
 }
