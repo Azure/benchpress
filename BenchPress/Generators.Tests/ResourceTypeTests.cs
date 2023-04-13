2de66fed64f0a0f49f7313617e87edf476673176
Caroline <caquigg@microsoft.com>
2023-04-12 20:09:20 -0700

    refact test metadata tests to resource type tests

---
+++ BenchPress/Generators.Tests/ResourceTypeTests.cs
@@ -0,0 +1,19 @@
+﻿using Generators.ResourceTypes;
+
+namespace Generators.Tests
+{
+    [TestClass]
+    public class ResourceTypeTests
+  {
+      [TestMethod]
+      public void Should_Throw_If_Unknown_ResourceType()
+      {
+          // arrange
+          var resourceType = "Unknown";
+
+          // act and assert
+          Action action = () => { ResourceType.Create(resourceType); };
+          Assert.ThrowsException<UnknownResourceTypeException>(action);
+      }
+  }
+}
