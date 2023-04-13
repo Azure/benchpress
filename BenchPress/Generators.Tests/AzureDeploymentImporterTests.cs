85c236da55bab59c8a8713cfe282b349ea3036bc
Caroline <caquigg@microsoft.com>
2023-04-13 14:26:23 -0700

    add shims for file io and static bicep functions

--- BenchPress/Generators.Tests/AzureDeploymentImporterTests.cs
+++ BenchPress/Generators.Tests/AzureDeploymentImporterTests.cs
@@ -1,51 +1,49 @@
-﻿using Generators.LanguageProviders;
+using System.IO.Fakes;
+using Bicep.Cli.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
 
 namespace Generators.Tests
 {
     [TestClass]
     public class AzureDeploymentImporterTests
-  {
-      [TestMethod]
-      public void Should_Throw_If_Invalid_File_Format()
-      {
-          // arrange
-          var inputFilePath = "in.txt";
-          var outputFilePath = "outFolder";
-
-          // act
-          Action action = () => { AzureDeploymentImporter.Import(inputFilePath, outputFilePath); };
-
-          // assert
-          Assert.ThrowsException<FileFormatException>(action);
-      }
-
+    {
       [TestMethod]
       public void Should_Succeed_With_Bicep_File()
       {
+        using (ShimsContext.Create())
+        {
+          ShimProgram.MainStringArray = (string[] input) =>
+          {
+            return Task.FromResult(0);
+          };
+
+          ShimFile.ReadAllTextString = (string input) =>
+          {
+            return "{\"resources\": [\r\n    {\r\n      \"type\": \"Microsoft.Resources/resourceGroups\",\r\n      \"apiVersion\": \"2022-09-01\",\r\n      \"name\": \"[parameters('name')]\",\r\n      \"location\": \"[parameters('location')]\"\r\n    }\r\n  ]}";
+          };
+
           // arrange
           var inputFilePath = "./DeploymentFiles/resourceGroup.bicep";
           var outputFilePath = "outFolder";
 
           // act
           var result = AzureDeploymentImporter.Import(inputFilePath, outputFilePath);
-
-          // assert
           Assert.IsNotNull(result);
+        }
       }
 
       [TestMethod]
-      public void Should_Succeed_With_Json_File()
+      public void Should_Throw_If_Invalid_File_Format()
       {
-          // arrange
-          var inputFilePath = "./DeploymentFiles/resourceGroup.json";
-          var outputFilePath = "outFolder";
+        // arrange
+        var inputFilePath = "in.txt";
+        var outputFilePath = "outFolder";
 
-          // act
-          var result = AzureDeploymentImporter.Import(inputFilePath, outputFilePath);
+        // act
+        Action action = () => { AzureDeploymentImporter.Import(inputFilePath, outputFilePath); };
 
-          // assert
-          Assert.IsNotNull(result);
+        // assert
+        Assert.ThrowsException<FileFormatException>(action);
       }
-
-  }
+    }
 }
--- BenchPress/Generators.Tests/TestGeneratorTests.cs
+++ BenchPress/Generators.Tests/TestGeneratorTests.cs
@@ -1,27 +1,37 @@
-﻿using Generators.LanguageProviders;
+using Generators.LanguageProviders;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.IO.Fakes;
 
 namespace Generators.Tests
 {
-    [TestClass]
-    public class TestGeneratorTests
+  [TestClass]
+  public class TestGeneratorTests
+  {
+    [TestMethod]
+    public void Should_Throw_If_Unknown_TestType()
     {
-        [TestMethod]
-        public void Should_Throw_If_Unknown_TestType()
+      using (ShimsContext.Create())
+      {
+        ShimFile.ReadAllTextString = (string path) =>
         {
-            // arrange
-            var resourceType = "Microsoft.Insights/actionGroups";
-            var resourceName = "Name";
-            var extraProperties = new Dictionary<string, string>();
+          return "template";
+        };
 
-            var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
-            var definition = new TestDefinition(metadata, (TestType)Int32.MaxValue);
+        // arrange
+        var resourceType = "Microsoft.Insights/actionGroups";
+        var resourceName = "Name";
+        var extraProperties = new Dictionary<string, string>();
 
-            var generator = new TestGenerator(new PowershellLanguageProvider());
-            var templateFile = "./templates/powershell/template.ps1";
+        var metadata = new TestMetadata(resourceType, resourceName, extraProperties);
+        var definition = new TestDefinition(metadata, (TestType)Int32.MaxValue);
 
-            // act and assert
-            Action action = () => { generator.Generate(new List<TestDefinition> { definition }, templateFile); };
-            Assert.ThrowsException<Exception>(action);
-        }
+        var generator = new TestGenerator(new PowershellLanguageProvider());
+        var templateFile = "./templates/powershell/template.ps1";
+
+        // act and assert
+        Action action = () => { generator.Generate(new List<TestDefinition> { definition }, templateFile); };
+        Assert.ThrowsException<Exception>(action);
+      }
     }
+  }
 }
