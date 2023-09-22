4a9ecafd21f7ca6b164582b2bb135b7f0fdda12b
polatengin <enpolat@microsoft.com>
2023-09-22 21:30:58 +0000

    adding negative test

--- Modules/BenchPress.Azure/Tests/Public/Confirm-Account.Tests.ps1
+++ Modules/BenchPress.Azure/Tests/Public/Confirm-Account.Tests.ps1
@@ -1,6 +1,7 @@
 ﻿BeforeAll {
   . $PSScriptRoot/../../Public/Confirm-ActionGroup.ps1
   . $PSScriptRoot/../../Private/Connect-Account.ps1
+  . $PSScriptRoot/../../Public/Confirm-Account.ps1
   Import-Module Az
 }
 
@@ -14,6 +15,20 @@ Describe "Confirm-Account" {
     It "Calls Get-AzContext" {
       Confirm-Account
       Should -Invoke -CommandName "Get-AzContext" -Times 1
+    }
+
+    It "Gets error when environment variables not set" {
+      $env:AZ_USE_MANAGED_IDENTITY = $false
+      $env:AZ_SUBSCRIPTION_ID = ""
+      $env:AZ_TENANT_ID = ""
+      $env:AZ_APPLICATION_ID = ""
+      $env:AZ_ENCRYPTED_PASSWORD = ""
+
+      $result = Confirm-Account
+
+      $result.Success | Should -Be $false
+      $result.AuthenticationData | Should -Be $null
+      $result.ResourceDetails | Should -Be $null
     }
   }
 }
